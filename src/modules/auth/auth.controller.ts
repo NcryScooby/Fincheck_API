3a650adb7d98d856327ab2b90f6a6de3fffa83f5
Fabrício <fabricio_c_anelli@hotmail.com>
2023-07-03 19:46:26 -0300

    feat: creating decorator IsPublic

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,20 +1,21 @@
-import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin';
 import { SignUpDto } from './dto/signup';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('signin')
-  @SetMetadata('IS_PUBLIC', true)
   signin(@Body() signInDto: SignInDto) {
     return this.authService.signin(signInDto);
   }
 
+  @IsPublic()
   @Post('signup')
-  @SetMetadata('IS_PUBLIC', true)
   signup(@Body() signUpDto: SignUpDto) {
     return this.authService.signup(signUpDto);
   }
