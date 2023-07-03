42c030bef9318e01472202a4e2615d45e96a7e06
Fabrício <fabricio_c_anelli@hotmail.com>
2023-07-03 19:40:09 -0300

    feat: adding metadata

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -7,6 +7,6 @@ export class UsersController {
 
   @Get('/user')
   users(@Req() request: any) {
-    return this.usersService.getUserById('userId');
+    return this.usersService.getUserById(request.userId);
   }
 }
